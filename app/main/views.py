7dcf575ff29c2b0173198c7f9ffe1eb2cc3e9616
Winston <wagawc@gmail.com>
2019-02-12 12:29:45 +0300

    add files to app/main/__init.py,view.py

---
+++ app/main/views.py
@@ -0,0 +1,22 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from ..models import Reviews, User
+
+@main.route('/')
+def index():
+
+    title = 'Home Page - Get The latest Pitch stories'
+    return render_template('index.html',title = title)
+
+# @main.route('/***/pitch/new', methods = ['GET','POST'])
+# @login_required
+# def new_pitch():
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
