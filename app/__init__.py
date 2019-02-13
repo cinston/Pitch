a4e5b45789629ffcb05cb32547886e3cf69fc3c3
Winston <wagawc@gmail.com>
2019-02-13 10:31:32 +0300

    fixed indentation error

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
-app = Flask(__name__)
+    app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
